2e4e2d3a17cfc35cba08655699b206809a20d063
Sai Kiran <saikiran40cs@users.noreply.github.com>
2018-10-05 12:08:59 +0530

    Create IOSHybridTest.java

---
+++ src/test/java/android/saucelabs/IOSHybridTest.java
@@ -0,0 +1,20 @@
+package android.saucelabs;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class IOSHybridTest extends SauceTestClass {
+
+    @Before
+    public void setUp() throws Exception {
+        capabilities.setCapability("deviceName", "iPhone Simulator");
+        capabilities.setCapability("platformVersion", "7.1");
+        capabilities.setCapability("app", getApp("HelloGappium.app.zip"));
+        super.setUpIosDriver();
+    }
+
+    @Test
+    public void basicIOSHybridTest() throws Exception {
+        runHybridTest();
+    }
+}
